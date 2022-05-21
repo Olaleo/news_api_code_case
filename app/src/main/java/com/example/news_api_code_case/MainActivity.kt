f1e324e061325bf90eb5bd75d76e7c0d6369ab9f
Olaleo <Aksel@jessens.org>
2022-05-21 08:46:37 +0200

    added splash screen

--- app/src/main/java/com/example/news_api_code_case/MainActivity.kt
+++ app/src/main/java/com/example/news_api_code_case/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.news_api_code_case.destinations.NavGraphs
@@ -29,6 +30,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             News_api_code_caseTheme {
